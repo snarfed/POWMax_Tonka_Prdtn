1427f52f08974b29024517808e5a38b40cc39a83
Richard Barrett <richard@rbshomes.com>
2022-12-29 16:09:02 -0800

    local_lib_test
    Test for mybinder operation.

---
+++ libtest.py
@@ -0,0 +1,9 @@
+# linenum fct for pynb library test
+def linenum(linenum):
+    print('          Here in fct inenum...')
+    if linenum == 1:
+        return '        1 is the answer.'
+    if linenum == 2:
+        return '        2 is the answer'
+    if linenum == 3:
+        return '        3 is the answer'
